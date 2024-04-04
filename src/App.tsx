afbb66b278e50e51d8896d17c8043fea9086b55d
Andrew Carey <andrew.r.carey@gmail.com>
2024-04-03 21:27:16 -0500

    basic router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Outlet, Link, Navigate } from "react-router-dom";
 import './App.css'
 
+//pages
+import AboutPage from './components/About/AboutPage';
+
+
 function App() {
+
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Navigate to='/home'></Navigate>}></Route>
+        <Route path='/home' element={content()}>        
+        </Route>
+        <Route path='/about' element={<AboutPage></AboutPage>}>
+        </Route>
+        <Route path='*' element={<h1>404</h1>}>
+        </Route>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+function content()  {
+
   const [count, setCount] = useState(0)
 
   return (
@@ -15,21 +40,19 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      </div><h1>Vite + React</h1><div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div><p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
+
 
 export default App
---
+++ src/components/About/AboutPage.tsx
@@ -0,0 +1,9 @@
+
+export default function AboutPage() {
+    return (
+        <>
+            <h1>About</h1>
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, vitae accusantium? Quidem soluta minus dolorum consectetur sed, reprehenderit molestias explicabo esse ut fugiat voluptatibus quo quam molestiae incidunt tenetur dignissimos!</p>
+        </>
+    )
+}
