b1affbe9407447c72176e8225baa376fc34a4dd3
Andrew Carey <andrew.r.carey@gmail.com>
2024-04-08 15:10:34 -0500

    fix mobile sidescroll layout bug

--- src/components/Main/Title-Card.tsx
+++ src/components/Main/Title-Card.tsx
@@ -2,12 +2,12 @@ import './Title-Card.css'
 
 export default function TitleCard() {
     return (
-        <>
+        <div>
             <div className='title-card-container'>
                 <h1 className='title-card-header'>Stay Informed. Every Day.</h1>
                 <h2 className='title-card-text'>Headlines written by professionals, stories written by the greatest latin text generators available.</h2>
                 <div className='divider title-card-divider'></div>
             </div>
-        </>
+        </div>
     )
 }
