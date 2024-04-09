59be29b32f7e2525aadbe3b1cd8b86a7dcc0c8ad
Andrew Carey <andrew.r.carey@gmail.com>
2024-04-08 19:40:56 -0500

    story card layout and dynamic sizing

--- src/components/Main/Story-Card.tsx
+++ src/components/Main/Story-Card.tsx
@@ -14,22 +14,23 @@ export default function StoryCard(props: Props) {
             <a className={props.storyIndex === 0 ? 'first-story story-card' : 'story-card'}
                 title={props.story.title}
                 href={`../story/${props.story.uri.split('/')[props.story.uri.split('/').length -1]}`}>
-                <div>
-                    <h3 className='story-card-title'>{props.story.title}</h3>
-                    <p className='story-card-byline'>{props.story.byline}</p>
-                    <div className='divider'></div>
-                </div>
+                <div className='story-card-content'>
+                    <div className='story-card-text'>
+                        <h3 className='story-card-title'>{props.story.title}</h3>
+                        <p className='story-card-byline'>{props.story.byline}</p>
+                    </div>
                 {props.story.multimedia && 
                     <img className='story-card-thumbnail' src={
-                        props.story.multimedia.filter(photo => photo.format === 'Large Thumbnail').length > 0 ?
-                        props.story.multimedia.filter(photo => photo.format === 'Large Thumbnail')[0].url :
-                        props.story.multimedia[props.story.multimedia.length - 1].url
+                        props.story.multimedia.filter(photo => photo.format === 'threeByTwoSmallAt2X').length > 0 ?
+                        props.story.multimedia.filter(photo => photo.format === 'threeByTwoSmallAt2X')[0].url :
+                        props.story.multimedia[props.story.multimedia.length - 2].url
                         }
                         alt={props.story.multimedia[props.story.multimedia.length - 1].caption}
                         ></img>
                 }
+                </div>
             </a>
-
+            <div className='divider story-card-divider'></div>
         </div>
     )
 }
