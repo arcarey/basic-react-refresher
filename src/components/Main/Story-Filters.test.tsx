e6094489849856c0a982e576172a495a7a938d31
Andrew Carey <andrew.r.carey@gmail.com>
2024-04-09 21:31:32 -0500

    test coverage for story detail and story filters

--- src/components/Main/Story-Filters.test.tsx
+++ src/components/Main/Story-Filters.test.tsx
@@ -1,1 +1,56 @@
+import { render, unmountComponentAtNode } from "react-dom";
+import StoryFilters from "./Story-Filters";
+import { act } from "react-dom/test-utils";
+import { SetStateAction } from "react";
+import { userEvent } from '@testing-library/user-event'
+
+
+
+let container: any = null;
+let mockActiveGenres = ['All']
+let mockGenres = ['us', 'health', 'business', 'nyregion', 'arts', 'opinion']
+function mockSetActiveGenre (value: SetStateAction<string[]>): any {
+    return value
+}
+
+describe( StoryFilters , () => {
+    beforeEach(() => {
+      container = document.createElement("div");
+      document.body.appendChild(container);
+    });
+  
+    afterEach(() => {
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
+    })
+  
+
+    it('renders the story filters with the All button active', () => {
+        act(() => {
+            render(<StoryFilters genres={mockGenres} activeGenres={['All']} setActiveGenres={mockSetActiveGenre} />, container);
+        });
+      expect(container.querySelector('.selected-button').textContent).toBe('All')
+    })
+
+    it('Sets active genre to All when clicked', async () => {
+        const user = userEvent.setup()
+        let testResult = ['']
+        const testSetActiveGenre = (value: any) => testResult = value ;
+        render(<StoryFilters genres={mockGenres} activeGenres={mockActiveGenres} setActiveGenres={testSetActiveGenre} />, container);
+        await user.click(container.querySelectorAll('button')[0])
+        await expect(testResult[0]).toBe('All');
+    })
+  
+    it('Sets active genre to us when clicked', async () => {
+        const user = userEvent.setup()
+        let testResult = ['']
+        const testSetActiveGenre = (value: any) => testResult = value ;
+        render(<StoryFilters genres={mockGenres} activeGenres={['arts']} setActiveGenres={testSetActiveGenre} />, container);
+        await user.click(container.querySelectorAll('button')[1])
+        await expect(testResult[0]).toBe('us');
+    })
+
+
+  })
 
